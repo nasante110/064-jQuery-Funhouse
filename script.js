709dd6b695e3e467aeafb412db93cbca13387ab8
nasante110 <105296949+nasante110@users.noreply.github.com>
2022-05-10 10:11:27 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(" .pokeball  ").click(function() {
+    $(".pikachu") .fadeIn ();
+    
+});
+
+$(" .light-switch  ").click(function() {
+$(".light-bulb") .toggle ();
+});
+
+$(" .hairclipper  ").click(function() {
+$(".shavedhead") .fadeToggle ();
+});
